27d89b02494c3474448073de15de1c0de4d6d84a
William Boone <138145899+boonewh@users.noreply.github.com>
2025-01-06 23:02:52 -0600

    Initial Commit. I shouldn't have waited this long.

---
+++ lotusrpg/routes.py
@@ -0,0 +1,79 @@
+from flask import render_template, url_for, flash, redirect, request
+from lotusrpg import app, db, bcrypt
+from lotusrpg.forms import RegistrationForm, LoginForm
+from lotusrpg.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/community')
+def community():
+    return render_template('community.html')
+
+@app.route('/forums')
+def forums():
+    return render_template('forums.html', posts=posts)
+
+@app.route('/charity')
+def charity():
+    return render_template('charity.html')
+
+@app.route('/sponsors')
+def sponsors():
+    return render_template('sponsors.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
