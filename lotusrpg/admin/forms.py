b9b4090f8aa7c318db2cb99cbc2a88734d0597e1
William Boone <138145899+boonewh@users.noreply.github.com>
2025-01-20 23:12:42 -0600

    fix import path for RuleForm

--- lotusrpg/admin/forms.py
+++ lotusrpg/admin/forms.py
@@ -2,12 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
-class SectionForm(FlaskForm):
+class RuleForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     slug = StringField('Slug', validators=[DataRequired()])
-    submit = SubmitField('Save Section')
-
-class ContentForm(FlaskForm):
     content_type = SelectField('Content Type', choices=[
         ('heading', 'Heading'),
         ('subheading', 'Subheading'),
@@ -15,8 +12,9 @@ class ContentForm(FlaskForm):
         ('table', 'Table'),
         ('list', 'List'),
         ('image', 'Image')
-    ], validators=[DataRequired()])
-    content_order = IntegerField('Content Order', validators=[DataRequired()])
-    content_data = TextAreaField('Content Data', validators=[DataRequired()])
+    ])
+    content_order = IntegerField('Content Order')
+    content = TextAreaField('Content', validators=[DataRequired()])
     style_class = StringField('Style Class')
-    submit = SubmitField('Save Content')
+    submit = SubmitField('Save')
+
