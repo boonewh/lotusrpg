72a4f3ac4138287450e17704bbb624ec938405cc
boonewh <boonewh@gmail.com>
2025-02-14 23:48:04 -0600

    Undid the changes made for Render and worked on the main page links and subscribe page

--- lotusrpg/users/routes.py
+++ lotusrpg/users/routes.py
@@ -31,3 +31,7 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', image_file=image_file, form=form)
+
+@users.route('/subscribe')
+def subscribe():
+    return render_template('subscribe.html')
